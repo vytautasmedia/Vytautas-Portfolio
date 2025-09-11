d07c5588374ea47d2f46b2280e5a785d820f0908
vytautasmedia <uvytas987@gmail.com>
2025-09-11 20:37:30 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import PrivatumoPolitika from './pages/PrivatumoPolitika'
+import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/privatumo-politika', element: <PrivatumoPolitika /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
