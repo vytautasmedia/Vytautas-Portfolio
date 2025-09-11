315a8fb7a515495b5f626abe194fb136a24df711
vytautasmedia <uvytas987@gmail.com>
2025-09-11 21:44:54 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -295,9 +295,8 @@ export default function App() {
 
         {/* HERO */}
         <section
-  id="hero"
-  className="border-b border-black/10 dark:border-white/10 bg-gradient-to-bl from-white to-gray-100"
->
+          id="hero"
+          className="border-b border-black/10 dark:border-white/10 bg-gradient-to-bl from-white to-blue-100">
           <div className="container py-16 md:py-24 grid md:grid-cols-2 gap-10 items-center">
             <motion.div initial={{ opacity: 0, y: 12 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6 }}>
               <h1 className="mt-4 text-4xl md:text-6xl font-bold leading-tight">{PROFILE.name}</h1>
